4e9131853673a47b460acc5dbdb59a67605782bf
John Clark <inindev@gmail.com>
2023-07-30 13:16:07 +0000

    convert to deb-dist-gen based
    Signed-off-by: John Clark <inindev@gmail.com>

--- debian/make_debian_img.sh
+++ debian/make_debian_img.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# Copyright (C) 2023, John Clark <inindev@gmail.com>
+
 set -e
 
 # script exit codes:
@@ -18,16 +20,18 @@ main() {
     local hostname='rock4cp-arm64'
     local acct_uid='debian'
     local acct_pass='debian'
-    local disable_ipv6=true
-    local extra_pkgs='curl, pciutils, sudo, u-boot-tools, wget, xxd, xz-utils, zip, zstd'
+    local extra_pkgs='curl, pciutils, sudo, unzip, wget, xxd, xz-utils, zip, zstd'
 
-    if is_param 'clean' $@; then
+    if is_param 'clean' "$@"; then
         rm -rf cache*/var
         rm -f "$media"*
+        rm -rf "$mountpt"
         rm -rf rootfs
         echo '\nclean complete\n'
         exit 0
     fi
+
+    check_installed 'wget' 'xz-utils'
 
     if [ -f "$media" ]; then
         read -p "file $media exists, overwrite? <y/N> " yn
@@ -38,7 +42,7 @@ main() {
     fi
 
     # no compression if disabled or block media
-    local compress=$(is_param 'nocomp' $@ || [ -b "$media" ] && echo false || echo true)
+    local compress=$(is_param 'nocomp' "$@" || [ -b "$media" ] && echo false || echo true)
 
     if $compress && [ -f "$media.xz" ]; then
         read -p "file $media.xz exists, overwrite? <y/N> " yn
@@ -48,49 +52,30 @@ main() {
         fi
     fi
 
-    check_installed 'debootstrap' 'u-boot-tools' 'wget' 'xz-utils'
-
     print_hdr "downloading files"
     local cache="cache.$deb_dist"
+
     # linux firmware
     local lfw=$(download "$cache" 'https://mirrors.edge.kernel.org/pub/linux/kernel/firmware/linux-firmware-20230210.tar.xz')
     local lfwsha='6e3d9e8d52cffc4ec0dbe8533a8445328e0524a20f159a5b61c2706f983ce38a'
+    [ "$lfwsha" = $(sha256sum "$lfw" | cut -c1-64) ] || { echo "invalid hash for $lfw"; exit 5; }
+
+    # u-boot
+    local uboot_spl=$(download "$cache" 'https://github.com/inindev/rockpi-4c-plus/releases/download/v12.0/idbloader.img')
+    [ -f "$uboot_spl" ] || { echo "unable to fetch $uboot_spl"; exit 4; }
+    local uboot_itb=$(download "$cache" 'https://github.com/inindev/rockpi-4c-plus/releases/download/v12.0/u-boot.itb')
+    [ -f "$uboot_itb" ] || { echo "unable to fetch: $uboot_itb"; exit 4; }
+
+    # dtb
+    local dtb=$(download "$cache" "https://github.com/inindev/rockpi-4c-plus/releases/download/v12.0/rk3399-rock-pi-4c-plus.dtb")
+    [ -f "$dtb" ] || { echo "unable to fetch $dtb"; exit 4; }
+
     # bluetooth firmware
     local bfw=$(download "$cache" 'https://github.com/murata-wireless/cyw-bt-patch/raw/master/BCM4345C0_003.001.025.0187.0366.1MW.hcd')
     local bfwsha='c903509c43baf812283fbd10c65faab3b0735e09bd57c5a9e9aa97cf3f274d3b'
-    # device tree & uboot
-    local dtb=$(download "$cache" 'https://github.com/inindev/rockpi-4c-plus/releases/download/v12.0/rk3399-rock-pi-4c-plus.dtb')
-#    local dtb='../dtb/rk3399-rock-pi-4c-plus.dtb'
-    local uboot_spl=$(download "$cache" 'https://github.com/inindev/rockpi-4c-plus/releases/download/v12.0/idbloader.img')
-#    local uboot_spl='../uboot/idbloader.img'
-    local uboot_itb=$(download "$cache" 'https://github.com/inindev/rockpi-4c-plus/releases/download/v12.0/u-boot.itb')
-#    local uboot_itb='../uboot/u-boot.itb'
+    [ "$bfwsha" = $(sha256sum "$bfw" | cut -c1-64) ] || { echo "invalid hash for $bfw"; exit 5; }
 
-    if [ "$lfwsha" != $(sha256sum "$lfw" | cut -c1-64) ]; then
-        echo "invalid hash for linux firmware: $lfw"
-        exit 5
-    fi
-
-    if [ "$bfwsha" != $(sha256sum "$bfw" | cut -c1-64) ]; then
-        echo "invalid hash for bluetooth firmware: $bfw"
-        exit 5
-    fi
-
-    if [ ! -f "$dtb" ]; then
-        echo "device tree binary is missing: $dtb"
-        exit 4
-    fi
-
-    if [ ! -f "$uboot_spl" ]; then
-        echo "unable to fetch uboot binary: $uboot_spl"
-        exit 4
-    fi
-
-    if [ ! -f "$uboot_itb" ]; then
-        echo "unable to fetch uboot binary: $uboot_itb"
-        exit 4
-    fi
-
+    # setup media
     if [ ! -b "$media" ]; then
         print_hdr "creating image file"
         make_image_file "$media"
@@ -102,7 +87,45 @@ main() {
     print_hdr "formatting media"
     format_media "$media"
 
+    print_hdr "mounting media"
     mount_media "$media"
+
+    print_hdr "configuring files"
+    mkdir "$mountpt/etc"
+    echo 'link_in_boot = 1' > "$mountpt/etc/kernel-img.conf"
+    echo 'do_symlinks = 0' >> "$mountpt/etc/kernel-img.conf"
+
+    # setup fstab
+    local mdev="$(findmnt -no source "$mountpt")"
+    local uuid="$(blkid -o value -s UUID "$mdev")"
+    echo "$(file_fstab $uuid)\n" > "$mountpt/etc/fstab"
+
+    # setup extlinux boot
+    install -Dm 754 'files/dtb_cp' "$mountpt/etc/kernel/postinst.d/dtb_cp"
+    install -Dm 754 'files/dtb_rm' "$mountpt/etc/kernel/postrm.d/dtb_rm"
+    install -Dm 754 'files/mk_extlinux' "$mountpt/boot/mk_extlinux"
+    ln -svf '../../../boot/mk_extlinux' "$mountpt/etc/kernel/postinst.d/update_extlinux"
+    ln -svf '../../../boot/mk_extlinux' "$mountpt/etc/kernel/postrm.d/update_extlinux"
+
+    print_hdr "installing firmware"
+    mkdir -p "$mountpt/usr/lib/firmware"
+    local lfwn=$(basename "$lfw")
+    local lfwbn="${lfwn%%.*}"
+    tar -C "$mountpt/usr/lib/firmware" --strip-components=1 --wildcards -xavf "$lfw" "$lfwbn/rockchip" "$lfwbn/rtl_nic" "$lfwbn/brcm/brcmfmac43455-sdio.AW-CM256SM.txt" "$lfwbn/cypress/cyfmac43455-sdio.*"
+
+    ln -svf "brcmfmac43455-sdio.AW-CM256SM.txt" "$mountpt/usr/lib/firmware/brcm/brcmfmac43455-sdio.radxa,rockpi4c-plus.txt"
+    ln -svf "../cypress/cyfmac43455-sdio.bin" "$mountpt/usr/lib/firmware/brcm/brcmfmac43455-sdio.radxa,rockpi4c-plus.bin"
+    ln -svf "../cypress/cyfmac43455-sdio.clm_blob" "$mountpt/usr/lib/firmware/brcm/brcmfmac43455-sdio.clm_blob"
+
+    local bfwn=$(basename "$bfw")
+    cp -v "$bfw" "$mountpt/usr/lib/firmware/brcm"
+    ln -svf "$bfwn" "$mountpt/usr/lib/firmware/brcm/BCM4345C0.radxa,rockpi4c-plus.hcd"
+
+    # install device tree
+    install -vm 644 "$dtb" "$mountpt/boot"
+
+    # install debian linux from deb packages (debootstrap)
+    print_hdr "installing root filesystem from debian.org"
 
     # do not write the cache to the image
     mkdir -p "$cache/var/cache" "$cache/var/lib/apt/lists"
@@ -110,10 +133,6 @@ main() {
     mount -o bind "$cache/var/cache" "$mountpt/var/cache"
     mount -o bind "$cache/var/lib/apt/lists" "$mountpt/var/lib/apt/lists"
 
-    # install debian linux from deb packages (debootstrap)
-    print_hdr "installing root filesystem from debian.org"
-    mkdir "$mountpt/etc"
-    echo 'link_in_boot = 1' > "$mountpt/etc/kernel-img.conf"
     local pkgs="linux-image-arm64, dbus, dhcpcd5, libpam-systemd, openssh-server, systemd-timesyncd"
     pkgs="$pkgs, wireless-regdb, wpasupplicant"
     pkgs="$pkgs, $extra_pkgs"
@@ -122,30 +141,14 @@ main() {
     umount "$mountpt/var/cache"
     umount "$mountpt/var/lib/apt/lists"
 
-    print_hdr "configuring files"
+    # apt sources & default locale
     echo "$(file_apt_sources $deb_dist)\n" > "$mountpt/etc/apt/sources.list"
     echo "$(file_locale_cfg)\n" > "$mountpt/etc/default/locale"
 
-    # disable sshd until after keys are regenerated on first boot
-    rm -f "$mountpt/etc/systemd/system/sshd.service"
-    rm -f "$mountpt/etc/systemd/system/multi-user.target.wants/ssh.service"
-    rm -f "$mountpt/etc/ssh/ssh_host_"*
-
-    # generate machine id on first boot
-    rm -f "$mountpt/etc/machine.id"
-    rm -f "$mountpt/etc/systemd/system/multi-user.target.wants/wpa_supplicant.service"
-
-    # wireless
+    # wpa supplicant
+    rm -rf "$mountpt/etc/systemd/system/multi-user.target.wants/wpa_supplicant.service"
     echo "$(file_wpa_supplicant_conf)\n" > "$mountpt/etc/wpa_supplicant/wpa_supplicant.conf"
     cp "$mountpt/usr/share/dhcpcd/hooks/10-wpa_supplicant" "$mountpt/usr/lib/dhcpcd/dhcpcd-hooks"
-
-    # misc cleanup
-    rm -f "$mountpt/etc/systemd/system/dbus-fi.w1.wpa_supplicant1.service"
-    rm -f "$mountpt/etc/systemd/system/dbus-org.freedesktop.timesync1.service"
-
-    # hostname
-    echo $hostname > "$mountpt/etc/hostname"
-    sed -i "s/127.0.0.1\tlocalhost/127.0.0.1\tlocalhost\n127.0.1.1\t$hostname/" "$mountpt/etc/hosts"
 
     # enable ll alias
     sed -i '/alias.ll=/s/^#*\s*//' "$mountpt/etc/skel/.bashrc"
@@ -154,38 +157,28 @@ main() {
     sed -i '/alias.l.=/s/^#*\s*//' "$mountpt/root/.bashrc"
 
     # motd (off by default)
-    is_param 'motd' $@ && [ -f '../etc/motd' ] && cp -f '../etc/motd' "$mountpt/etc"
+    is_param 'motd' "$@" && [ -f '../etc/motd' ] && cp -f '../etc/motd' "$mountpt/etc"
 
-    # setup /boot
-    echo "$(script_boot_txt $disable_ipv6)\n" > "$mountpt/boot/boot.txt"
-    mkimage -A arm64 -O linux -T script -C none -n 'u-boot boot script' -d "$mountpt/boot/boot.txt" "$mountpt/boot/boot.scr"
-    echo "$(script_mkscr_sh)\n" > "$mountpt/boot/mkscr.sh"
-    chmod 754 "$mountpt/boot/mkscr.sh"
-    install -m 644 "$dtb" "$mountpt/boot"
-    ln -sf $(basename "$dtb") "$mountpt/boot/dtb"
-
-    print_hdr "installing firmware"
-    mkdir -p "$mountpt/lib/firmware"
-    local lfwn=$(basename "$lfw")
-
-    tar -C "$mountpt/lib/firmware" --strip-components=1 --wildcards -xavf "$lfw" "${lfwn%%.*}/rockchip" "${lfwn%%.*}/rtl_nic" "${lfwn%%.*}/brcm/brcmfmac43455-sdio.AW-CM256SM.txt" "${lfwn%%.*}/cypress/cyfmac43455-sdio.*"
-    ln -sf brcmfmac43455-sdio.AW-CM256SM.txt "$mountpt/lib/firmware/brcm/brcmfmac43455-sdio.radxa,rockpi4c-plus.txt"
-    ln -sf ../cypress/cyfmac43455-sdio.bin "$mountpt/lib/firmware/brcm/brcmfmac43455-sdio.radxa,rockpi4c-plus.bin"
-    ln -sf ../cypress/cyfmac43455-sdio.clm_blob "$mountpt/lib/firmware/brcm/brcmfmac43455-sdio.clm_blob"
-
-    local bfwn=$(basename "$bfw")
-    cp "$bfw" "$mountpt/lib/firmware/brcm"
-    ln -sf "$bfwn" "$mountpt/lib/firmware/brcm/BCM4345C0.radxa,rockpi4c-plus.hcd"
-
-    print_hdr "installing rootfs expansion script to /etc/rc.local"
-    echo "$(script_rc_local)\n" > "$mountpt/etc/rc.local"
-    chmod 754 "$mountpt/etc/rc.local"
+    # hostname
+    echo $hostname > "$mountpt/etc/hostname"
+    sed -i "s/127.0.0.1\tlocalhost/127.0.0.1\tlocalhost\n127.0.1.1\t$hostname/" "$mountpt/etc/hosts"
 
     print_hdr "creating user account"
-    chroot "$mountpt" /usr/sbin/useradd -m $acct_uid -s /bin/bash
+    chroot "$mountpt" /usr/sbin/useradd -m "$acct_uid" -s '/bin/bash'
     chroot "$mountpt" /bin/sh -c "/usr/bin/echo $acct_uid:$acct_pass | /usr/sbin/chpasswd -c YESCRYPT"
-    chroot "$mountpt" /usr/bin/passwd -e $acct_uid
+    chroot "$mountpt" /usr/bin/passwd -e "$acct_uid"
     (umask 377 && echo "$acct_uid ALL=(ALL) NOPASSWD: ALL" > "$mountpt/etc/sudoers.d/$acct_uid")
+
+    print_hdr "installing rootfs expansion script to /etc/rc.local"
+    install -Dm 754 'files/rc.local' "$mountpt/etc/rc.local"
+
+    # disable sshd until after keys are regenerated on first boot
+    rm -f "$mountpt/etc/systemd/system/sshd.service"
+    rm -f "$mountpt/etc/systemd/system/multi-user.target.wants/ssh.service"
+    rm -f "$mountpt/etc/ssh/ssh_host_"*
+
+    # generate machine id on first boot
+    rm -f "$mountpt/etc/machine.id"
 
     # reduce entropy on non-block media
     [ -b "$media" ] || fstrim -v "$mountpt"
@@ -217,7 +210,7 @@ make_image_file() {
     local filename="$1"
     rm -f "$filename"*
     local size="$(echo "$filename" | sed -rn 's/.*mmc_([[:digit:]]+[m|g])\.img$/\1/p')"
-    truncate -s $size "$filename"
+    truncate -s "$size" "$filename"
 }
 
 parition_media() {
@@ -251,13 +244,11 @@ format_media() {
     fi
 }
 
-
 mount_media() {
     local media="$1"
     local partnum="1"
 
     if [ -d "$mountpt" ]; then
-        echo "cleaning up mount points..."
         mountpoint -q "$mountpt/var/cache" && umount "$mountpt/var/cache"
         mountpoint -q "$mountpt/var/lib/apt/lists" && umount "$mountpt/var/lib/apt/lists"
         mountpoint -q "$mountpt" && umount "$mountpt"
@@ -290,8 +281,7 @@ mount_media() {
 }
 
 check_mount_only() {
-    local img
-    local flag=false
+    local item img flag=false
     for item in "$@"; do
         case "$item" in
             mount) flag=true ;;
@@ -311,7 +301,7 @@ check_mount_only() {
     fi
 
     if [ "$img" = *.xz ]; then
-        tmp=$(basename "$img" .xz)
+        local tmp=$(basename "$img" .xz)
         if [ -f "$tmp" ]; then
             echo "compressed file ${bld}$img${rst} was specified but uncompressed file ${bld}$tmp${rst} exists..."
             echo -n "mount ${bld}$tmp${rst}"
@@ -341,33 +331,34 @@ check_mount_only() {
     exit 0
 }
 
-# download / return file from cache
-download() {
-    local cache="$1"
-    local url="$2"
+# ensure inner mount points get cleaned up
+on_exit() {
+    if mountpoint -q "$mountpt"; then
+        mountpoint -q "$mountpt/var/cache" && umount "$mountpt/var/cache"
+        mountpoint -q "$mountpt/var/lib/apt/lists" && umount "$mountpt/var/lib/apt/lists"
 
-    [ -d "$cache" ] || mkdir -p "$cache"
-
-    local filename="$(basename "$url")"
-    local filepath="$cache/$filename"
-    [ -f "$filepath" ] || wget "$url" -P "$cache"
-    [ -f "$filepath" ] || exit 2
-
-    echo "$filepath"
-}
-
-# check if utility program is installed
-check_installed() {
-    local todo
-    for item in "$@"; do
-        dpkg -l "$item" 2>/dev/null | grep -q "ii  $item" || todo="$todo $item"
-    done
-
-    if [ ! -z "$todo" ]; then
-        echo "this script requires the following packages:${bld}${yel}$todo${rst}"
-        echo "   run: ${bld}${grn}sudo apt update && sudo apt -y install$todo${rst}\n"
-        exit 1
+        read -p "$mountpt is still mounted, unmount? <Y/n> " yn
+        if [ -z "$yn" -o "$yn" = 'y' -o "$yn" = 'Y' -o "$yn" = 'yes' -o "$yn" = 'Yes' ]; then
+            echo "unmounting $mountpt"
+            umount "$mountpt"
+            sync
+            rm -rf "$mountpt"
+        fi
     fi
+}
+mountpt='rootfs'
+trap on_exit EXIT INT QUIT ABRT TERM
+
+file_fstab() {
+    local uuid="$1"
+
+    cat <<-EOF
+	# if editing the device name for the root entry, it is necessary
+	# to regenerate the extlinux.conf file by running /boot/mk_extlinux
+
+	# <device>					<mount>	<type>	<options>		<dump> <pass>
+	UUID=$uuid	/	ext4	errors=remount-ro	0      1
+	EOF
 }
 
 file_apt_sources() {
@@ -377,14 +368,14 @@ file_apt_sources() {
 	# For information about how to configure apt package sources,
 	# see the sources.list(5) manual.
 
-	deb http://deb.debian.org/debian $deb_dist main contrib non-free non-free-firmware
-	#deb-src http://deb.debian.org/debian $deb_dist main contrib non-free non-free-firmware
+	deb http://deb.debian.org/debian ${deb_dist} main contrib non-free non-free-firmware
+	#deb-src http://deb.debian.org/debian ${deb_dist} main contrib non-free non-free-firmware
 
-	deb http://deb.debian.org/debian-security $deb_dist-security main contrib non-free non-free-firmware
-	#deb-src http://deb.debian.org/debian-security $deb_dist-security main contrib non-free non-free-firmware
+	deb http://deb.debian.org/debian-security ${deb_dist}-security main contrib non-free non-free-firmware
+	#deb-src http://deb.debian.org/debian-security ${deb_dist}-security main contrib non-free non-free-firmware
 
-	deb http://deb.debian.org/debian $deb_dist-updates main contrib non-free non-free-firmware
-	#deb-src http://deb.debian.org/debian $deb_dist-updates main contrib non-free non-free-firmware
+	deb http://deb.debian.org/debian ${deb_dist}-updates main contrib non-free non-free-firmware
+	#deb-src http://deb.debian.org/debian ${deb_dist}-updates main contrib non-free non-free-firmware
 	EOF
 }
 
@@ -415,116 +406,51 @@ file_locale_cfg() {
 	EOF
 }
 
-script_rc_local() {
-    cat <<-EOF
-	#!/bin/sh
+# download / return file from cache
+download() {
+    local cache="$1"
+    local url="$2"
 
-	set -e
+    [ -d "$cache" ] || mkdir -p "$cache"
 
-	this=\$(realpath \$0)
-	perm=\$(stat -c %a \$this)
+    local filename="$(basename "$url")"
+    local filepath="$cache/$filename"
+    [ -f "$filepath" ] || wget "$url" -P "$cache"
+    [ -f "$filepath" ] || exit 2
 
-	if [ 774 -eq \$perm ]; then
-	    # expand fs
-	    resize2fs "\$(findmnt -no source /)"
-	    rm "\$this"
-	    systemctl stop rc-local.service
-	else
-	    # regen ssh keys
-	    dpkg-reconfigure openssh-server
-	    systemctl enable ssh.service
-
-	    # expand root parition & change uuid
-	    rp="\$(findmnt -no source /)"
-	    rpn="\$(echo "\$rp" | grep -Eo '[[:digit:]]*\$')"
-	    rd="/dev/\$(lsblk -no pkname "\$rp")"
-	    uuid="\$(cat /proc/sys/kernel/random/uuid)"
-	    echo "size=+, uuid=\$uuid" | sfdisk -f -N "\$rpn" "\$rd"
-
-	    # change rootfs uuid
-	    uuid="\$(cat /proc/sys/kernel/random/uuid)"
-	    echo "changing rootfs uuid: \$uuid"
-	    tune2fs -U "\$uuid" "\$rp"
-
-	    # setup for expand fs
-	    chmod 774 "\$this"
-	    reboot
-	fi
-	EOF
-}
-
-script_boot_txt() {
-    local no_ipv6="$($1 && echo ' ipv6.disable=1')"
-
-    cat <<-EOF
-	# after modifying, run ./mkscr.sh
-
-	part uuid \${devtype} \${devnum}:\${distro_bootpart} uuid
-	setenv bootargs console=ttyS2,1500000 root=PARTUUID=\${uuid} rw rootwait$no_ipv6 earlycon=uart8250,mmio32,0xff1a0000
-
-	if load \${devtype} \${devnum}:\${distro_bootpart} \${kernel_addr_r} /boot/vmlinuz; then
-	    if load \${devtype} \${devnum}:\${distro_bootpart} \${fdt_addr_r} /boot/dtb; then
-	        fdt addr \${fdt_addr_r}
-	        fdt resize
-	        if load \${devtype} \${devnum}:\${distro_bootpart} \${ramdisk_addr_r} /boot/initrd.img; then
-	            booti \${kernel_addr_r} \${ramdisk_addr_r}:\${filesize} \${fdt_addr_r};
-	        else
-	            booti \${kernel_addr_r} - \${fdt_addr_r};
-	        fi;
-	    fi;
-	fi
-	EOF
-}
-
-script_mkscr_sh() {
-    cat <<-EOF
-	#!/bin/sh
-
-	if [ ! -x /usr/bin/mkimage ]; then
-	    echo 'mkimage not found, please install uboot tools:'
-	    echo '  sudo apt -y install u-boot-tools'
-	    exit 1
-	fi
-
-	mkimage -A arm64 -O linux -T script -C none -n 'u-boot boot script' -d boot.txt boot.scr
-	EOF
+    echo "$filepath"
 }
 
 is_param() {
-    local match
-    for item in $@; do
-        if [ -z $match ]; then
-            match=$item
-        elif [ $match = $item ]; then
-            return
+    local item match
+    for item in "$@"; do
+        if [ -z "$match" ]; then
+            match="$item"
+        elif [ "$match" = "$item" ]; then
+            return 0
         fi
     done
-    false
+    return 1
+}
+
+# check if debian package is installed
+check_installed() {
+    local item todo
+    for item in "$@"; do
+        dpkg -l "$item" 2>/dev/null | grep -q "ii  $item" || todo="$todo $item"
+    done
+
+    if [ ! -z "$todo" ]; then
+        echo "this script requires the following packages:${bld}${yel}$todo${rst}"
+        echo "   run: ${bld}${grn}sudo apt update && sudo apt -y install$todo${rst}\n"
+        exit 1
+    fi
 }
 
 print_hdr() {
-    local msg=$1
+    local msg="$1"
     echo "\n${h1}$msg...${rst}"
 }
-
-# ensure inner mount points get cleaned up
-on_exit() {
-    if mountpoint -q "$mountpt"; then
-        print_hdr "cleaning up mount points"
-        mountpoint -q "$mountpt/var/cache" && umount "$mountpt/var/cache"
-        mountpoint -q "$mountpt/var/lib/apt/lists" && umount "$mountpt/var/lib/apt/lists"
-
-        read -p "$mountpt is still mounted, unmount? <Y/n> " yn
-        if [ -z "$yn" -o "$yn" = 'y' -o "$yn" = 'Y' -o "$yn" = 'yes' -o "$yn" = 'Yes' ]; then
-            echo "unmounting $mountpt"
-            umount "$mountpt"
-            sync
-            rm -rf "$mountpt"
-        fi
-    fi
-}
-mountpt='rootfs'
-trap on_exit EXIT INT QUIT ABRT TERM
 
 rst='\033[m'
 bld='\033[1m'
